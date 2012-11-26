c512bbbfceb994fd1b4873dda4fd5b3d33a479e9
ktmud <jyyjcc@gmail.com>
2012-11-26 14:04:01 +0800

    more dialogs

--- rules/waits.js
+++ rules/waits.js
@@ -10,7 +10,16 @@ var cities = data.cities;
 var webot = require('weixin-robot');
 var waiter = webot.waiter();
 
-waiter.set('who_create', {
+waiter.set('who_create1', {
+  pattern: /你是.+做的/,
+  tip: '我其实是一个很猥琐的程序员做的，要我把他的微信号告诉你吗？',
+  'replies': {
+    'Y': '好的，他的微信帐号是：YjgxNTQ5ZmQzYTA0OWNjNTQ3NzliNGMyNzRmYjdhMTUK',
+    'N': '可惜了啊，其实他还长得蛮帅的' 
+  }
+});
+
+waiter.set('who_create2', {
   pattern: function(info) {
     var reg = /(什么人|谁|哪位.*)(给|为|帮)?你?(设置|做|配置|制造|制作|设计|创造|生产?)(了|的)?/;
     return reg.test(info.text) && info.text.replace(reg, '').indexOf('你') === 0;
