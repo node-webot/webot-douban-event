6bceab6ddbe9dbd3a39fd2930ec8adb7fe8c00de
ktmud <jyyjcc@gmail.com>
2012-11-26 15:25:29 +0800

    more dialog

--- data/index.js
+++ data/index.js
@@ -4,7 +4,7 @@ module.exports = {
   'types': require('./types'),
   'day_types': {
     'future': '近期|将来',
-    'week': '最近一周|本周|最近|一周',
+    'week': '最近一周|本周|最近|一周|这个星期|这星期',
     'weekend': '周末',
     'today': '今天|今日',
     'tomorrow': '明天|明日'
