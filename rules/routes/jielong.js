2c27b061e41a1ae21027e4d19e33f061e2631f85
ktmud <jyyjcc@gmail.com>
2012-11-28 22:00:26 +0800

    more dialog

--- rules/routes/jielong.js
+++ rules/routes/jielong.js
@@ -2,12 +2,27 @@ var pwd = process.cwd();
 var chengyu = require(pwd + '/data').chengyu;
 
 var reg_punc = /[。\.\s…\!]/g;
+
+function pick(obj) {
+    var result;
+    var count = 0;
+    for (var prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
+}
+
 // 成语接龙
 module.exports = {
   'pattern': function(info) {
+    if (info.text === '成语' || info.text === '成语接龙') {
+      info.reply = pick(chengyu.explain);
+      return true;
+    }
     return info.text && (info.text.replace(reg_punc, '') in chengyu.explain);
   },
   'handler': function(info) {
+    if (info.reply) return info.reply;
     var lastChar = info.text[info.text.length - 1]; 
     if (lastChar in chengyu.index) {
       var ret = chengyu.index[lastChar].sample(1)[0];
