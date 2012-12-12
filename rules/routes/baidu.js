9c591b93139acf956ef6f7122f8bbf3a1be60c82
ktmud <jyyjcc@gmail.com>
2012-12-12 11:15:22 +0800

    baidu too long

--- rules/routes/baidu.js
+++ rules/routes/baidu.js
@@ -12,13 +12,13 @@ function do_search(info, next) {
     // 调用 toString 方法，转换为 utf-8 的字符串
     res = res.toString();
 
-    var reg_h3t = /<h3 class="t">(.+?)<\/h3>/gi;
+    var reg_h3t = /<h3 class="t">\s*(.+?)\s*<\/h3>/gi;
     var links = [];
     var i = 1;
 
     while (true) {
       var m = reg_h3t.exec(res);
-      if (!m) break;
+      if (!m || i > 5) break;
       links.push(i + '. ' + m[1]);
       i++;
     }
@@ -26,6 +26,9 @@ function do_search(info, next) {
     var ret;
     if (links.length) {
       ret = '在百度搜索到以下结果：\n\n' + links.join('\n');
+      ret = ret.replace(/\s*data-click=".*?"/gi,  '');
+      ret = ret.replace(/<em>(.*?)<\/em>/gi,  '$1');
+      console.log(ret.length);
     } else {
       ret = '搜不到任何结果呢';
     }
