4c9c126f1b38284f282f550e35abf9ea92c0f482
Jesse Yang <jyyjcc@gmail.com>
2013-05-14 17:14:09 +0800

    羽泉活动

--- rules/index.js
+++ rules/index.js
@@ -14,6 +14,16 @@ webot.set(/^建议(.{4})/, function(info) {
   info.flag = true;
   return '你的意见已经收到，我们会尽快处理。[微笑]';
 });
+webot.set('羽泉',{
+  pattern: function(info) {
+    return (/我.*羽泉/).test(info.text) && info.u.getLoc() == '108288';
+  },
+  handler: function(info) {
+    info.flag = true;
+    return '你已成功参加《羽泉15周年特别版音乐会》北京抢票活动，如果中奖我们会通过微信直接联系你。\n\n'
+    + '<a href="http://www.douban.com/location/partner/event/18875660/">活动详情»</a>';
+  }
+});
 
 require('js-yaml');
 
