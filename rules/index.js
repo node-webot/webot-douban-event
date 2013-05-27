5bd690cb15f1c12f1f1dee21844aada04113d5b7
Jesse Yang <jyyjcc@gmail.com>
2013-05-27 10:00:26 +0800

    寻找初恋

--- rules/index.js
+++ rules/index.js
@@ -14,14 +14,14 @@ webot.set(/^建议(.{4})/, function(info) {
   info.flag = true;
   return '你的意见已经收到，我们会尽快处理。[微笑]';
 });
-webot.set('羽泉',{
+webot.set('寻找初恋',{
   pattern: function(info) {
-    return (/我.*羽泉/).test(info.text) && info.u.getLoc() == '108288';
+    return (/报名.*寻找初恋/).test(info.text) && info.u.getLoc() == '108296';
   },
   handler: function(info) {
     info.flag = true;
-    return '你已成功参加《羽泉15周年特别版音乐会》北京抢票活动，如果中奖我们会通过微信直接联系你。\n\n\n'
-    + '<a href="http://www.douban.com/location/partner/event/18875660/">活动详情»</a>';
+    return '你需要在豆瓣同城上找到「《寻找初恋》专业观众观摩场」活动，点击「我要参加」填写报名表，提供姓名和电话。如果被选中，我们会电话联系你。'
+    + '<a href="http://www.douban.com//event/18928228/">活动详情»</a>';
   }
 });
 
